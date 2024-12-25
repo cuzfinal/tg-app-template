2cf6d3c35c16cc95df73c422d0326c1e3256c7ad
cuzfinal <s929459939@gmail.com>
2024-12-25 17:38:14 +0800

    mockEnv

--- src/init.ts
+++ src/init.ts
@@ -12,41 +12,47 @@ import {
 /**
  * Initializes the application and configures its dependencies.
  */
-export function initMiniApp(): void {
+export function initMiniApp() {
 	// Check if all required components are supported.
-	if (!backButton.isSupported() || !miniApp.isSupported()) {
-		return
+	// if (!backButton.isSupported() || !miniApp.isSupported()) {
+	// 	return
+	// }
+
+	try {
+		const searchParams = retrieveLaunchParams()
+		const debug = searchParams.startParam === 'debug' || import.meta.env.DEV
+
+		// Set @telegram-apps/sdk-react debug mode.
+		$debug.set(debug)
+
+		// Initialize special event handlers for Telegram Desktop, Android, iOS, etc.
+		// Also, configure the package.
+		initSDK()
+
+		// Add Eruda if needed.
+		debug &&
+			import('eruda').then((lib) => lib.default.init()).catch(console.error)
+
+		// Mount all components used in the project.
+		backButton.mount()
+		miniApp.mount()
+		themeParams.mount()
+		initData.restore()
+		void viewport
+			.mount()
+			.catch((e) => {
+				console.error('Something went wrong mounting the viewport', e)
+			})
+			.then(() => {
+				viewport.bindCssVars()
+			})
+
+		// Define components-related CSS variables.
+		miniApp.bindCssVars()
+		themeParams.bindCssVars()
+		return true
+	} catch (e) {
+		console.error(e)
+		return false
 	}
-
-	const searchParams = retrieveLaunchParams()
-	const debug = searchParams.startParam === 'debug' || import.meta.env.DEV
-
-	// Set @telegram-apps/sdk-react debug mode.
-	$debug.set(debug)
-
-	// Initialize special event handlers for Telegram Desktop, Android, iOS, etc.
-	// Also, configure the package.
-	initSDK()
-
-	// Add Eruda if needed.
-	debug &&
-		import('eruda').then((lib) => lib.default.init()).catch(console.error)
-
-	// Mount all components used in the project.
-	backButton.mount()
-	miniApp.mount()
-	themeParams.mount()
-	initData.restore()
-	void viewport
-		.mount()
-		.catch((e) => {
-			console.error('Something went wrong mounting the viewport', e)
-		})
-		.then(() => {
-			viewport.bindCssVars()
-		})
-
-	// Define components-related CSS variables.
-	miniApp.bindCssVars()
-	themeParams.bindCssVars()
 }
